3d5f27863a4206f36822b012dc250be091b41295
Alexey Kuznetsov <42695162+paltusplintus@users.noreply.github.com>
2022-06-09 07:13:49 +0200

    windows voila init setup (failed)

---
+++ app.bat
@@ -0,0 +1,1 @@
+venv\Scripts\voila app.ipynb
---
+++ setup.bat
@@ -0,0 +1,3 @@
+pip install -r requirements.txt
+jupyter nbextension enable --py --sys-prefix ipyvuetify
+jupyter serverextension enable voila --sys-prefix
